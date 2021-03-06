4b666704f3191e5462b944305667b2d12094d470
alessandro.verrecchia <alessandro.verrecchia@quebecormedia.com>
2021-03-06 15:06:41 -0500

    basic quick links fragment

--- app/src/main/java/com/alessandro/magichelper/MainActivity.kt
+++ app/src/main/java/com/alessandro/magichelper/MainActivity.kt
@@ -2,18 +2,20 @@ package com.alessandro.magichelper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.alessandro.magichelper.quickLinks.QuickLinksFragment
 import com.alessandro.magichelper.scoreBoard.ScoreBoardFragment
 import com.alessandro.magichelper.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
     private val searchFragment = SearchFragment()
     private val scoreBoardFragment = ScoreBoardFragment()
+    private val quickLinksFragment = QuickLinksFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, scoreBoardFragment)
+                .replace(R.id.container, quickLinksFragment)
                 .commitNow()
         }
     }
---
+++ app/src/main/java/com/alessandro/magichelper/quickLinks/QuickLinksFragment.kt
@@ -0,0 +1,71 @@
+package com.alessandro.magichelper.quickLinks
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.alessandro.magichelper.R
+import kotlinx.android.synthetic.main.fragment_quick_links.*
+
+
+class QuickLinksFragment : Fragment() {
+
+    companion object {
+        private const val GOLD_FISH_META_GAME = "https://www.mtggoldfish.com/metagame/modern#paper"
+        private const val STAR_CITY_GAMES = "https://starcitygames.com/"
+        private const val CHANNEL_FIREBALL = "https://shop.channelfireball.com/"
+        private const val FACE_TO_FACE_GAMES = "https://www.facetofacegames.com/"
+        private const val TOP_DECK_HERO = "http://www.topdeckhero.com/"
+        private const val JEUX_3_DRAGON = "https://www.jeux3dragons.com/"
+        private const val MTG_TOP_8 = "https://mtgtop8.com/"
+        private const val TCG_PLAYER = "https://www.tcgplayer.com/"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_quick_links, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewClickListeners()
+    }
+
+    private fun setupViewClickListeners() {
+        cfbTextView.setOnClickListener {
+            openUrl(CHANNEL_FIREBALL)
+        }
+        scgTextView.setOnClickListener {
+            openUrl(STAR_CITY_GAMES)
+        }
+        tcgpTextView.setOnClickListener {
+            openUrl(TCG_PLAYER)
+        }
+        j3dTextView.setOnClickListener {
+            openUrl(JEUX_3_DRAGON)
+        }
+        tdhTextView.setOnClickListener {
+            openUrl(TOP_DECK_HERO)
+        }
+        goldfishTextView.setOnClickListener {
+            openUrl(GOLD_FISH_META_GAME)
+        }
+        mtgt8TextView.setOnClickListener {
+            openUrl(MTG_TOP_8)
+        }
+        ftfTextView.setOnClickListener {
+            openUrl(FACE_TO_FACE_GAMES)
+        }
+    }
+
+    private fun openUrl(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
+
+}
