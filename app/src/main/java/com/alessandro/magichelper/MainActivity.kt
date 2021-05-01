3d725a932edf1fd5b8def70f0ae12390a570edde
alessandro.verrecchia <alessandro.verrecchia@quebecormedia.com>
2021-04-30 20:42:24 -0400

    added bottom navigation between fragments

--- app/src/main/java/com/alessandro/magichelper/MainActivity.kt
+++ app/src/main/java/com/alessandro/magichelper/MainActivity.kt
@@ -2,21 +2,30 @@ package com.alessandro.magichelper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.alessandro.magichelper.quickLinks.QuickLinksFragment
 import com.alessandro.magichelper.scoreBoard.ScoreBoardFragment
 import com.alessandro.magichelper.search.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val searchFragment = SearchFragment()
-    private val scoreBoardFragment = ScoreBoardFragment()
-    private val quickLinksFragment = QuickLinksFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, quickLinksFragment)
-                .commitNow()
+        val navController = findNavController(R.id.navHostFragment).apply {
+            graph.startDestination = R.id.scoreBoardFragment
+        }
+        bottomNavView.setupWithNavController(navController)
+        handleBottomNavItemReselection(navController)
+    }
+
+    private fun handleBottomNavItemReselection(navController: NavController) {
+        bottomNavView.setOnNavigationItemReselectedListener { menuItem ->
+            navController.navigate(menuItem.itemId)
         }
     }
 }
